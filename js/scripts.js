730d39d79ddcef5b4e286683e8f2b403e4ad008e
Cesar Madera Garces <camgcamg11@gmail.com>
2024-03-16 10:29:47 -0500

    adding opacity to background image

--- js/scripts.js
+++ js/scripts.js
@@ -39,7 +39,12 @@ window.addEventListener('DOMContentLoaded', async event => {
         <section class="resume-section" id="${review['name']}" style="background-image: url('./assets/img/${review['name']}/ktana.png');background-size: contain;background-repeat: no-repeat;opacity: .5;">
                 <div class="resume-section-content">
                     <h2 class="mb-5 text-primary">${review['name']}</h2>
-                    <zero-md src="${'https://raw.githubusercontent.com/cesar214567/food-reviews/main/mds/' + review['md']}"></zero-md>
+                    <zero-md src="${'https://raw.githubusercontent.com/cesar214567/food-reviews/main/mds/' + review['md']}">
+                        <template>
+                        <!-- The CSS load order is respected -->
+                        <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@4.3.1/dist/css/bootstrap.min.css" integrity="sha384-ggOyR0iXCbMQv3Xipma34MD+dH/1fQ784/j6cY/iJTQUOhcWr7x9JvoRxT2MZw1T" crossorigin="anonymous">
+                        </template>
+                    </zero-md>
                 </div>
             </section>
             <hr class="m-0" />
